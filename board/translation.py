2706d5c1975c1dbd2e2d7bd7cc605a4af5d78259
elenakhudyakova <114878302+elenakhudyakova@users.noreply.github.com>
2023-07-17 13:25:49 +0300

    Delete board directory

--- board/translation.py
+++
@@ -1,7 +0,0 @@
-from .models import Category
-from modeltranslation.translator import register, TranslationOptions
-
-
-@register(Category)
-class CategoryTranslationOptions(TranslationOptions):
-    fields = ('name', )
